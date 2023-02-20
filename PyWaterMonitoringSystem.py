76ab70d146f9099081b19c1dd3b9b02998127411
MaxTurner-cloud <61795751+MaxTurner-cloud@users.noreply.github.com>
2023-02-20 11:40:49 -0500

    Rev13 Commit Working With ATM-Breakout

--- PyWaterMonitoringSystem.py
+++ PyWaterMonitoringSystem.py
@@ -1,13 +1,12 @@
 from __future__ import print_function
 import paho.mqtt.publish as publish
-import pyMultiSerial as p   # Create object of class pyMultiSerial
+import pyMultiSerial as p  # Create object of class pyMultiSerial
 import serial
 import qwiic_bme280
 import sys
 import datetime
 import time
 import requests
-
 
 ms = p.MultiSerial()
 ms.baudrate = 9600  # open serial port at 9600 to match Arduino's
@@ -18,6 +17,11 @@ date = str(datetime.datetime.now())
 # Callback function on detecting a port connection.
 # Parameters: Port Number, Serial Port Object
 # Return: True if the port is to be accepted, false if the port is to be rejected based on some condition within library
+# For MQTT: publish multiple messages - this is a Python list of dict elements!
+# topic parts: "emon" is required; "Sprinkler1" is a Node-name; "Moisture1" "GPM1", etc. are data labels
+
+# Publish them via MQTT using the multiple method
+# Do not need to specify a host since publishing internally
 
 # Check if there are any ports open and if so continue
 def port_connection_found_callback(portno, serial):
@@ -43,7 +47,7 @@ def port_read_callback(portno, serial, text):
     # here im going to parse the text data coming in and turn it into ints then scale as necessary. Some data
     # is already scaled in the Arduino's such as percentages because it's not a lot of load for the arduino
     with open('portread.txt', '+w') as w:  # Open new file to store data only for each call back in order to read
-        w.write(text + "\n")        # writing data to file without appending
+        w.write(text + "\n")  # writing data to file without appending
 
     with open('portread.txt', 'r') as r:
         readline = r.readline()
@@ -52,42 +56,60 @@ def port_read_callback(portno, serial, text):
 
     # using the pointer set the correct data set to the data following the pointer
     if read_key == "gmcA":
-        gmcA = str(readline[4: 10])
-        print("true1 " + str(gmcA))
+        gmcA = str(readline[4: 10])  # read Data coming in
+        print("true1 " + str(gmcA))  # print for testing purposes
+        # Send via MQTT
         msg = [{'topic': "emon/Sprinkler1/Moisture1", 'payload': float(gmcA)}]
         publish.multiple(msg, auth={'username': "emonpi", 'password': "emonpimqtt2016"})
 
     if read_key == "gmcB":
-        gmcB = str(readline[4: 10])
-        print("true2 " + str(gmcB))
+        gmcB = str(readline[4: 10])  # read Data coming in
+        print("true2 " + str(gmcB))  # print for testing purposes
+        # Send via MQTT
         msg = [{'topic': "emon/Sprinkler1/Moisture2", 'payload': float(gmcB)}]
         publish.multiple(msg, auth={'username': "emonpi", 'password': "emonpimqtt2016"})
 
     if read_key == "preA":
-        preA = str(readline[4: 10])
-        print("true3 " + str(preA))
+        preA = str(readline[4: 10])  # read Data coming in
+        print("true3 " + str(preA))  # print for testing purposes
+        # Send via MQTT
         msg = [{'topic': "emon/Sprinkler1/Pressure1", 'payload': float(preA)}]
-        publish.multiple(msg, auth={'username': "emonpi", 'password': "emonpimqtt2016"})
-
-    if read_key == "preB":
-        preB = str(readline[4: 10])
-        print("true4" + str(preB))
-        msg = [{'topic': "emon/Sprinkler1/Pressure2", 'payload': float(preB)}]
         publish.multiple(msg, auth={'username': "emonpi", 'password': "emonpimqtt2016"})
 
     else:  # if no data found then return out and look for more
         print("No data was found. looking for more")
 
     breakout_sensor()  # calls for function breakout sensor
+    with open('atmBreakoutRead.txt', 'r') as a:
+        for x in a:
+            read_line_break = x
+            # read first 10 characters of the file to get the pointer to the data
+            read_key_break = read_line_break[0: 4]
+            print(read_key_break)
 
-    # publish multiple messages - this is a Python list of dict elements!
-    # topic parts: "emon" is required; "Sprinkler1" is a Node-name; "Moisture1" "GPM1", etc. are data labels
+            if read_key_break == "preO":
+                preO = str(readline[4: 10])  # read Data coming in
+                print("true5" + str(preO))  # print for testing purposes
+                # Send via MQTT
+                msg = [{'topic': "emon/ATMBreakout/Pressure", 'payload': float(preO)}]
+                publish.multiple(msg, auth={'username': "emonpi", 'password': "emonpimqtt2016"})
 
-    # Publish them via MQTT using the multiple method
-    # Do not need to specify a host since publishing internally
+            if read_key_break == "temp":
+                temp = str(readline[4: 10])  # read Data coming in
+                print("true6" + str(temp))  # print for testing purposes
+                # Send via MQTT
+                msg = [{'topic': "emon/ATMBreakout/Humidity", 'payload': float(temp)}]
+                publish.multiple(msg, auth={'username': "emonpi", 'password': "emonpimqtt2016"})
 
-    # calls for function breakout sensor while sending the data in as args
-    # emon_send(gmcA, gmcB, preA, preB, humidity, atm_pressure, temperature)
+            if read_key_break == "humd":
+                humd = str(readline[4: 10])  # read Data coming in
+                print("true7" + str(humd))  # print for testing purposes
+                # Send via MQTT
+                msg = [{'topic': "emon/ATMBreakout/Humidity", 'payload': float(humd)}]
+                publish.multiple(msg, auth={'username': "emonpi", 'password': "emonpimqtt2016"})
+
+            else:  # if no data found then return out and look for more
+                print("No data was found. looking for more")
 
 
 # register callback function
@@ -107,60 +129,28 @@ ms.port_disconnection_callback = port_disconnection_callback
 def breakout_sensor():
     mySensor = qwiic_bme280.QwiicBme280()
     mySensor.begin()  # start atm breakout sensor
+
     if mySensor.connected:
-        humidity = ("Humidity:\t%.3f" % (float(mySensor.humidity)+10.0))  # find humidity
-        atm_pressure = ("Pressure:\t%.3f" % mySensor.pressure)  # find atmospheric pressure
-        temperature = ("Temperature:\t%.2f" % mySensor.temperature_fahrenheit)  # find temperature
+        hum_scaled = (int(mySensor.humidity) + 10.0)
+        humidity = ("humd" + str(hum_scaled))  # find humidity
+        atm_pressure = ("preO" + str(mySensor.pressure))  # find atmospheric pressure
+        temperature = ("temp" + str(mySensor.temperature_fahrenheit))  # find temperature
         time.sleep(2)  # force slowdown so pi doesn't get backed up and crash
-        print(humidity, atm_pressure, temperature)  # print for debugging purpose
-        with open('atmBreakout.txt', '+a') as f:  # write text to file with append
-            f.write(date)
-            f.write(str(humidity) + "\n")
+        # print(humidity, atm_pressure, temperature)  # print for debugging purpose
+        with open('atmBreakout.txt', '+a') as f:  # write text to file with append to store data
+            f.write(humidity + "\n")
             f.write(atm_pressure + "\n")
             f.write(temperature + "\n")
-            f.write("\n")
+        with open('atmBreakoutRead.txt', 'w') as i:     # write to file while writing over old text for MQTT send
+            i.write(humidity + "\n")
+            i.write(atm_pressure + "\n")
+            i.write(temperature + "\n")
         return
+
     if not mySensor.connected:
         print("The Qwiic BME280 device isn't connected to the system. Please check your connection",
               file=sys.stderr)
         return
-
-
-# send data to Emoncms using data put into dictionary (key-value pairs)
-def emon_send(gmcA, gmcB, preA, preB, humidity, atm_pressure, temperature):
-    thisdict = {
-        "ground moisture 1": gmcA,
-        "ground moisture 2": gmcB,
-        "Water Pressure 1": preA,
-        "Water Pressure 2": preB,
-        "Humidity": humidity,
-        "Atmospheric Pressure": atm_pressure
-    }
-    print(thisdict)
-
-    # Finalized readings to send to Emon
-    moisture1 = gmcA
-    moisture2 = gmcB
-    pressure1 = preA
-    pressure2 = preB
-    GPM1 = 211
-    Temperature1 = temperature
-    Rotation1_RPM = 0.3
-
-    # publish multiple messages - this is a Python list of dict elements!
-    # topic parts: "emon" is required; "Sprinkler1" is a Node-name; "Moisture1" "GPM1", etc. are data labels
-    msg = [{'topic': "emon/Sprinkler1/Moisture1", 'payload': moisture1},
-           {'topic': "emon/Sprinkler1/Moisture2", 'payload': moisture2},
-           {'topic': "emon/Sprinkler1/Pressure1", 'payload': pressure1},
-           {'topic': "emon/Sprinkler1/Pressure2", 'payload': pressure2},
-           {'topic': "emon/Sprinkler1/GPM1", 'payload': GPM1},
-           {'topic': "emon/Sprinkler1/Temperature1", 'payload': Temperature1},
-           {'topic': "emon/Sprinkler1/Rotation1_RPM", 'payload': Rotation1_RPM}]
-
-    # Publish them via MQTT using the multiple method
-    # Do not need to specify a host since publishing internally
-    publish.multiple(msg, auth={'username': "emonpi", 'password': "emonpimqtt2016"})
-    return
 
 
 # Start Monitoring ports
